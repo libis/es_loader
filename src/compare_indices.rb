9d2ed0e6113d59038e097cdc51f563be15715a66
Tom Vanmechelen <tom.vanmechelen@libis.be>
2025-09-19 10:50:52 +0200

    compare between installations

--- src/compare_indices.rb
+++ src/compare_indices.rb
@@ -5,12 +5,53 @@ require_relative './lib/loader'
 
 ##########
 
-INDEX1 = "icandid_20240618"
-INDEX2 = "icandid_20250101"
+SOURCE_URL = "https://icandid_admin:ibandid_admin@host.docker.internal:9292"
+SOURCE_INDEX = "icandid_20250606"
+TARGET_URL = "https://icandid_admin:ibandid_admin@host.docker.internal:9222"
+TARGET_INDEX = "icandid"
 
 #field_to_compare = "isBasedOn.provider.@id"
 field_to_compare = "isBasedOn.isPartOf.@id"
  
+
+# Helper to scroll and collect all IDs
+def fetch_all_ids(client: nil, index: nil, body: { _source: false })
+  ids = Set.new
+  response = client.search(index: index, scroll: '2m', size: 1000, body: body)
+
+  scroll_id = response['_scroll_id']
+  hits = response['hits']['hits']
+  ids.merge(hits.map { |doc| doc['_id'] })
+
+  while hits.any?
+    response = client.scroll(scroll_id: scroll_id, scroll: '2m')
+    scroll_id = response['_scroll_id']
+    hits = response['hits']['hits']
+    ids.merge(hits.map { |doc| doc['_id'] })
+  end
+
+  ids
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 begin
   
 
@@ -18,73 +59,62 @@ begin
   # every docker-container has its own HOSTNAME
   output_dir = "/elastic/import/#{ENV["HOSTNAME"]}"
   output_file = "import.bulk"
-
-  #Create a loader object.
-  loader = Loader.new()
-  config = loader.config()
-
-  loader.logger.info "use utils for es_cluster : #{loader.es_cluster}"
-
-  loader.check_elastic()
-  
-prefixes = [  
-"iCANDID_zweedsparlement_data_riksdagen_query_0000001_h",
-"iCANDID_zweedsparlement_data_riksdagen_query_0000001_ha",
-"iCANDID_zweedsparlement_data_riksdagen_query_0000001_hb",
-"iCANDID_zweedsparlement_data_riksdagen_query_0000001_hc",
-"iCANDID_zweedsparlement_data_riksdagen_query_0000001_h6",
-"iCANDID_zweedsparlement_data_riksdagen_query_0000001_h8",
-"iCANDID_zweedsparlement_data_riksdagen_query_0000001_h9",
-"iCANDID_zweedsparlement_data_riksdagen_query_0000001_HA1",
-"iCANDID_zweedsparlement_data_riksdagen_query_0000001_HA",
-"iCANDID_zweedsparlement_data_riksdagen_query_0000001_HB",
-"iCANDID_zweedsparlement_data_riksdagen_query_0000001_H6",
-"iCANDID_zweedsparlement_data_riksdagen_query_0000001_H7",
-"iCANDID_zweedsparlement_data_riksdagen_query_0000001_H8",
-"iCANDID_zweedsparlement_data_riksdagen_query_0000001_H9",
-"iCANDID_zweedsparlement_data_riksdagen_query_0000001_H9A",
-"iCANDID_zweedsparlement_data_riksdagen_query_0000001_H9B",
-"iCANDID_zweedsparlement_data_riksdagen_query_0000001_H9D",
-"iCANDID_zweedsparlement_data_riksdagen_query_0000001_H90",
-"iCANDID_zweedsparlement_data_riksdagen_query_0000001_H91",
-"iCANDID_zweedsparlement_data_riksdagen_query_0000001_s",
-"iCANDID_zweedsparlement_data_riksdagen_query_0000001_z",
-"iCANDID_zweedsparlement_data_riksdagen_query_0000001_Z"
-]
-
-=begin
-prefixes.each  { |prefix| 
-
-  result_1 = loader.search(index: INDEX1, body: { size: 0, track_total_hits: true, query: { prefix: { "@id": { value: prefix } } } })
-  result_2 = loader.search(index: INDEX2, body: { size: 0, track_total_hits: true, query: { prefix: { "@id": { value: prefix } } }})
-
-  #GET INDEX1/_search?filter_path=hits.total
-  if ( result_1["hits"]["total"]["value"] != result_2["hits"]["total"]["value"] )
-    pp prefix
-    pp "#{result_1["hits"]["total"]["value"]} #{result_2["hits"]["total"]["value"]}"
+  begin
+    @es_source_client = Elasticsearch::Client.new url: SOURCE_URL, transport_options: {  ssl:  { verify: false }, request: { timeout: 120 } }
+    @es_target_client = Elasticsearch::Client.new url: TARGET_URL, transport_options: {  ssl:  { verify: false }, request: { timeout: 120 } }
+  rescue => e
+    puts "Error creating Elasticsearch::Client: #{e.message}"
   end
 
-}
-=end
+  body = { size: 0, track_total_hits: true, aggs: { field_to_compare: { terms: { field: field_to_compare, size: 1000, order: { _key: "asc" } } } } }
 
-  # client.search(index: 'my_index', body: { query: { match_all: {} } })
-  result_1 = loader.search(index: INDEX1, body: { size: 0, track_total_hits: true, aggs: { field_to_compare: { terms: { field: field_to_compare, size: 1000, order: { _key: "asc" } } } } })
-  result_2 = loader.search(index: INDEX2, body: { size: 0, track_total_hits: true, aggs: { field_to_compare: { terms: { field: field_to_compare, size: 1000, order: { _key: "asc" } } } } })
+  begin
+    source_results = @es_source_client.search ({index: SOURCE_INDEX, body: body } )
+  rescue => e
+    puts "Error searching source : #{e.message}"
+  end
 
-  pp "--------------------------------------------"
-  pp ""
+  begin
+    target_results = @es_target_client.search ({index: TARGET_INDEX, body: body } )
+  rescue => e
+    puts "Error searching target : #{e.message}"
+  end
 
-  result_1["aggregations"]["field_to_compare"]["buckets"].each { |agg_1| 
-    agg_2 = result_2["aggregations"]["field_to_compare"]["buckets"].select { |s| s["key"] ==  agg_1["key"]}.first
-    if agg_2.nil?
+  source_results["aggregations"]["field_to_compare"]["buckets"].each { |source_result| 
+    target_result = target_results["aggregations"]["field_to_compare"]["buckets"].select { |s| s["key"] ==  source_result["key"]}.first
+    if target_result.nil?
       #pp "#{agg_1} is missing in #{INDEX2}"
     else
-      if agg_1[ "doc_count"] == agg_2[ "doc_count"]
-        pp " #{agg_1["key"]} : #{agg_1[ "doc_count"] } OK"
+      if source_result[ "doc_count"] == target_result[ "doc_count"]
+        pp " #{source_result["key"]} : #{source_result[ "doc_count"] } OK"
       else
-        pp "==============>  Not the same amount of records for #{agg_1["key"]}"
-        pp "   #{agg_1["key"]} : #{agg_1[ "doc_count"] } records (#{INDEX1})"
-        pp "   #{agg_2["key"]} : #{agg_2[ "doc_count"] } records (#{INDEX2})"
+        pp "==============>  Not the same amount of records for #{source_result["key"]}"
+        pp "   #{source_result["key"]} : #{source_result[ "doc_count"] } records (#{SOURCE_URL}/#{SOURCE_INDEX})"
+        pp "   #{target_result["key"]} : #{target_result[ "doc_count"] } records (#{TARGET_URL}/#{TARGET_INDEX})"
+
+        body = { _source: false, query: { terms: { "#{field_to_compare}": [ "#{target_result["key"]}" ] } }}
+
+        pp body
+        if target_result["key"] == "belgapress_query_00026"
+          # Fetch IDs
+          puts "Fetching IDs from #{SOURCE_URL}/#{SOURCE_INDEX} ..."
+          source_ids = fetch_all_ids(client: @es_source_client, index: SOURCE_INDEX, body: body)
+
+          puts "Fetching IDs from #{TARGET_URL}/#{TARGET_INDEX} ..."
+          target_ids = fetch_all_ids(client: @es_target_client, index: TARGET_INDEX, body: body)
+
+          # Compare
+          missing_ids = source_ids - target_ids
+
+          # Output
+          puts "\nMissing IDs in #{TARGET_URL}/#{TARGET_INDEX}:"
+          missing_ids.each { |id| puts id }
+
+          puts "\nTotal missing: #{missing_ids.size}"
+
+       exit
+        end
+
       end
     end
   
@@ -99,13 +129,6 @@ rescue StandardError => e
 
   pp e
   exit
-
-ensure
-
-
-  pp "--> ENSURE "
-  exit
-
 
 end
 
